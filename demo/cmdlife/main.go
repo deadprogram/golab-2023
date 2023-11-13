67da452ff97326b54f84e658ebc2df84674cc7bb
deadprogram <ron@hybridgroup.com>
2023-11-13 19:10:40 +0100

    demo/cmdlife: simplify
    Signed-off-by: deadprogram <ron@hybridgroup.com>

--- demo/cmdlife/main.go
+++ demo/cmdlife/main.go
@@ -1,26 +1,24 @@
 package main
 
 import (
-	"flag"
+	"bufio"
+	"os"
 
 	"github.com/acifani/vita/lib/game"
 )
 
 func main() {
-	height := flag.Int("height", 32, "height of the universe")
-	width := flag.Int("width", 32, "width of the universe")
-	generations := flag.Int("gens", 3, "how many generations to run the universe")
-	population := flag.Int("pop", 45, "initial population percent of the universe")
+	universe := game.NewUniverse(16, 16)
+	universe.Randomize(45)
 
-	flag.Parse()
-
-	universe := game.NewUniverse(uint32(*height), uint32(*width))
-	universe.Randomize(*population)
-
-	for i := 0; i < *generations; i++ {
+	for {
+		println("Generation", universe.Generation)
 		println(universe.String())
 		println()
-		
+
 		universe.Tick()
+
+		println("Press enter to continue")
+		bufio.NewReader(os.Stdin).ReadBytes('\n')
 	}
 }
