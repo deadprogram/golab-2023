8cf90f6a65f2fff4610938a2c31d8ff39e9f2b5a
deadprogram <ron@hybridgroup.com>
2023-11-19 08:38:06 +0100

    demo/cubelife: cleanup order for connecting universes
    Signed-off-by: deadprogram <ron@hybridgroup.com>

--- demo/cubelife/main.go
+++ demo/cubelife/main.go
@@ -108,8 +108,8 @@ func createUniverses() []*game.ParallelUniverse {
 	for i := 0; i < 6; i++ {
 		u := game.NewParallelUniverse(height, width)
 		u.Randomize(population)
-		multi = append(multi, u)
 
+		multi = append(multi, u)
 		gamebuffers = append(gamebuffers, make([]byte, height*width))
 	}
 
@@ -118,40 +118,34 @@ func createUniverses() []*game.ParallelUniverse {
 
 func connectUniverses(multi []*game.ParallelUniverse) {
 	multi[0].SetTopNeighbor(multi[3])
-	multi[3].SetBottomNeighbor(multi[0])
-
-	multi[0].SetRightNeighbor(multi[1])
-	multi[1].SetLeftNeighbor(multi[0])
-
 	multi[0].SetBottomNeighbor(multi[3])
-	multi[3].SetTopNeighbor(multi[0])
-
 	multi[0].SetLeftNeighbor(multi[2])
-	multi[2].SetRightNeighbor(multi[0])
+	multi[0].SetRightNeighbor(multi[1])
 
 	multi[1].SetTopNeighbor(multi[4])
-	multi[4].SetBottomNeighbor(multi[1])
-
-	multi[1].SetRightNeighbor(multi[2])
-	multi[2].SetLeftNeighbor(multi[1])
-
 	multi[1].SetBottomNeighbor(multi[4])
-	multi[4].SetTopNeighbor(multi[1])
+	multi[1].SetLeftNeighbor(multi[0])
+	multi[1].SetRightNeighbor(multi[2])
 
 	multi[2].SetTopNeighbor(multi[5])
-	multi[5].SetBottomNeighbor(multi[2])
-
 	multi[2].SetBottomNeighbor(multi[5])
-	multi[5].SetTopNeighbor(multi[2])
+	multi[2].SetLeftNeighbor(multi[1])
+	multi[2].SetRightNeighbor(multi[0])
 
+	multi[3].SetTopNeighbor(multi[0])
+	multi[3].SetBottomNeighbor(multi[0])
 	multi[3].SetLeftNeighbor(multi[5])
-	multi[5].SetRightNeighbor(multi[3])
-
 	multi[3].SetRightNeighbor(multi[4])
-	multi[4].SetLeftNeighbor(multi[3])
 
+	multi[4].SetTopNeighbor(multi[1])
+	multi[4].SetBottomNeighbor(multi[1])
+	multi[4].SetLeftNeighbor(multi[3])
 	multi[4].SetRightNeighbor(multi[5])
+
+	multi[5].SetTopNeighbor(multi[2])
+	multi[5].SetBottomNeighbor(multi[2])
 	multi[5].SetLeftNeighbor(multi[4])
+	multi[5].SetRightNeighbor(multi[3])
 }
 
 func runUniverses(multi []*game.ParallelUniverse) {
